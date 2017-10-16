29eea81bb8784c1ce7579c1d3ce07152e4c3e392
Bryan Phelps <bryphe@outlook.com>
2017-10-16 11:44:37 -0700

    Initial gatsby generation

---
+++ gatsby-config.js
@@ -0,0 +1,70 @@
+module.exports = {
+  siteMetadata: {
+    title: `My website`
+  },
+  mapping: {
+    'MarkdownRemark.frontmatter.author': `AuthorJson`
+  },
+  plugins: [
+    // Expose `/data` to graphQL layer
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/data`
+      }
+    },
+
+    // Parse all markdown files (each plugin add/parse some data into graphQL layer)
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 690,
+              backgroundColor: `#f7f0eb`
+            }
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-autolink-headers`
+        ]
+      }
+    },
+
+    // Parse all images files
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+
+    // Parse JSON files
+    `gatsby-transformer-json`,
+
+    // Add typescript stack into webpack
+    `gatsby-plugin-typescript`,
+
+    // This plugin takes your configuration and generates a
+    // web manifest file so your website can be added to your
+    // homescreen on Android.
+    /* eslint-disable camelcase */
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby website`,
+        short_name: `Gatsby website`,
+        start_url: `/`,
+        background_color: `#f7f7f7`,
+        theme_color: `#191919`,
+        display: `minimal-ui`
+      }
+    },
+    /* eslint-enable camelcase */
+
+    // This plugin generates a service worker and AppShell
+    // html file so the site works offline and is otherwise
+    // resistant to bad networks. Works with almost any
+    // site!
+    `gatsby-plugin-offline`
+  ]
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import Link from "gatsby-link";
+import HeaderMenu from "../components/HeaderMenu/HeaderMenu";
+import { menuItems } from "../layouts";
+import {
+  Button,
+  Segment,
+  Container,
+  Grid,
+  Header,
+  Icon,
+} from "semantic-ui-react";
+
+interface IndexPageProps {
+  location: {
+    pathname: string;
+  };
+}
+
+const logo = require("./oni-header.png");
+
+// import logo from "./oni-header.png"
+
+export default (props: IndexPageProps) =>
+  <div>
+    {/* Master head */}
+    <Segment vertical textAlign="center" className="masthead">
+      <HeaderMenu
+        Link={Link} pathname={props.location.pathname} items={menuItems}
+      />
+      <Container text>
+        <img src={logo} alt="Oni Logo" />
+        <Header as="h2">Modern IDE for (neo)vim</Header>
+        <Button primary size="huge">Download</Button>
+      </Container>
+    </Segment>
+  </div>;
