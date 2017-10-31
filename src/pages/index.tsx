a2332135cbc594ccbfb19bb690ae1df4fb3fb925
Bryan Phelps <bryphe@outlook.com>
2017-10-30 19:09:31 -0700

    Add carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,39 +12,83 @@ interface IndexPageProps {
 const logo = require("./logo-256x256.png");
 const header = require("./oni-header.png");
 
-
 const createDivItem = (className: string) => {
     return class SingleClassItem extends React.PureComponent<{}, {}> {
         public render(): JSX.Element {
-            return <div className={className}>{this.props.children}</div>
+            return <div className={className}>{this.props.children}</div>;
         }
-    }
-}
+    };
+};
 
 const createAnchorItem = (className: string) => {
     return class HOC extends React.PureComponent<{href: string }, {}> {
         public render(): JSX.Element {
-            return <a className={className} href={this.props.href}>{this.props.children}</a>
+            return <a className={className} href={this.props.href}>{this.props.children}</a>;
         }
-    }
-}
+    };
+};
 const GitHubIconLarge = () => {
                         return <span className="icon" style={{color: "#333" }}>
         <i className="fa fa-lg fa-github">
         </i>
-    </span>
-}
+    </span>;
+};
 
 const TwitterIconLarge = () => {
                         return <span className="icon" style={{color: "#55acee" }}>
         <i className="fa fa-lg fa-twitter">
         </i>
-    </span>
+    </span>;
+};
+
+const NavBarItem = createAnchorItem("navbar-item");
+const NavBarItemHiddenOnDesktop = createAnchorItem("navbar-item is-hidden-desktop");
+const NavBarItemDesktopOnly = createAnchorItem("navbar-item is-hidden-desktop-only");
+
+import { Carousel } from "react-responsive-carousel";
+const styles = require("react-responsive-carousel/lib/styles/carousel.min.css");
+
+export interface IHeroState {
+    isRendered: boolean;
 }
 
-const NavBarItem = createAnchorItem("navbar-item")
-const NavBarItemHiddenOnDesktop = createAnchorItem("navbar-item is-hidden-desktop")
-const NavBarItemDesktopOnly = createAnchorItem("navbar-item is-hidden-desktop-only")
+export class HeroImageSlider extends React.PureComponent<{}, IHeroState> {
+
+    constructor() {
+        super();
+        this.state = {
+            isRendered: false,
+        };
+    }
+
+    public componentDidMount(): void {
+        // window.setTimeout(() => {
+        // this.setState({
+        //     isRendered: true
+        // })
+        // }, 1000)
+    }
+
+    public render(): JSX.Element {
+        const settings = {
+            dots: true,
+            infinite: false,
+            speed: 500,
+            slidesToShow: 0,
+            slidesToScroll: 1,
+        };
+
+        // if (!this.state.isRendered) {
+        //     return <div />
+        // }
+
+        return <Carousel showThumbs={false} showIndicators={false}>
+                <div><img src="https://user-images.githubusercontent.com/13532591/28976286-25779704-78f2-11e7-967f-72cb438d77f6.png" /><h2>Showcase 1</h2></div>
+                <div><img src="https://user-images.githubusercontent.com/13532591/28976286-25779704-78f2-11e7-967f-72cb438d77f6.png" /></div>
+                <div><img src="https://user-images.githubusercontent.com/13532591/28976286-25779704-78f2-11e7-967f-72cb438d77f6.png" /></div>
+            </Carousel>;
+    }
+}
 
 const NavBarMenu = () => {
 
@@ -70,9 +114,24 @@ const NavBarMenu = () => {
                 <TwitterIconLarge />
             </NavBarItemDesktopOnly>
         </div>
-    </div>
+    </div>;
 
-}
+};
+
+const FeatureCard = () => {
+
+    return <div className="card">
+        <div className="card-header">
+            <div className="card-header-title">
+                Modern UX
+            </div>
+        </div>
+        <div className="card-content">
+            This is content about the feature
+        </div>
+    </div>;
+
+};
 
 // import logo from "./oni-header.png"
 
@@ -106,14 +165,14 @@ export default (props: IndexPageProps) => {
         <section className="hero is-dark is-medium">
             <div className="hero-body">
                 <div className="columns">
-                    <div className="column">
-                        <a className="button is-primary is-large">Download</a>
+                    <div className="column oni-flex-center">
+                        <a className="oni-button button is-primary is-large">Download</a>
                     </div>
-                    <div className="column">
-                        <img src="https://user-images.githubusercontent.com/13532591/28976286-25779704-78f2-11e7-967f-72cb438d77f6.png" />
+                    <div className="column oni-flex-center">
+                        <HeroImageSlider />
                     </div>
-                    <div className="column">
-                        <a className="button is-info is-large">Support</a>
+                    <div className="column oni-flex-center">
+                        <a className="oni-button button is-info is-large">Support</a>
                     </div>
                 </div>
             </div>
@@ -121,6 +180,17 @@ export default (props: IndexPageProps) => {
         <section className="section is-light">
             <div className="container">
                 <h1 className="title">Features</h1>
+                <div className="columns is-8">
+                    <div className="column">
+                        <FeatureCard />
+                    </div>
+                    <div className="column">
+                        <FeatureCard />
+                    </div>
+                    <div className="column">
+                        <FeatureCard />
+                    </div>
+                </div>
             </div>
         </section>
         <section className="section is-light">
@@ -133,5 +203,5 @@ export default (props: IndexPageProps) => {
                 <h1 className="title">Learn More</h1>
             </div>
         </section>
-    </div>
-}
+    </div>;
+};
