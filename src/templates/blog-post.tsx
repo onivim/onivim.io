0e989155ca015be713c0536fb47dfa4509dcf594
Bryan Phelps <bryphe@outlook.com>
2018-02-05 20:00:12 -0800

    Get static blogs working

---
+++ src/templates/blog-post.tsx
@@ -0,0 +1,154 @@
+import * as React from "react";
+import Link from "gatsby-link";
+import { Header, Container, Segment, Icon, Label, Button, Grid, Card, Image, Item, Comment } from "semantic-ui-react";
+import { MarkdownRemark, ImageSharp, MarkdownRemarkConnection } from "../graphql-types";
+import BlogTitle from "../components/BlogTitle";
+
+import { NavBar } from "./../components/NavBar"
+
+interface BlogPostProps {
+  data: {
+    post: MarkdownRemark;
+    recents: MarkdownRemarkConnection;
+  };
+}
+
+export default (props: BlogPostProps) => {
+  const { frontmatter, html, timeToRead } = props.data.post;
+  const avatar = frontmatter.author.avatar.children[0] as ImageSharp;
+
+  // const tags = props.data.post.frontmatter.tags
+  //   .map((tag) => <Label key={tag}><Link to={`/blog/tags/${tag}/`}>{tag}</Link></Label>);
+
+  const recents = props.data.recents.edges
+    .map(({ node }) => {
+      const recentAvatar = node.frontmatter.author.avatar.children[0] as ImageSharp;
+      const extra = (
+        <Comment.Group>
+          <Comment>
+            <Comment.Avatar
+              src={recentAvatar.responsiveResolution.src}
+              srcSet={recentAvatar.responsiveResolution.srcSet}
+            />
+            <Comment.Content>
+              <Comment.Author style={{ fontWeight: 400 }}>
+                {node.frontmatter.author.id}
+              </Comment.Author>
+              <Comment.Metadata style={{ margin: 0 }}>
+                {node.timeToRead} min read
+              </Comment.Metadata>
+            </Comment.Content>
+          </Comment>
+        </Comment.Group>
+      );
+
+      return (
+        <div key={node.fields.slug} style={{paddingBottom: "1em"}}>
+        </div>
+      );
+    });
+
+  return (
+    <Container>
+      <NavBar logo={""} />
+      <BlogTitle />
+      <Segment vertical
+        style={{ border: "none" }}
+        dangerouslySetInnerHTML={{
+          __html: html,
+        }}
+      />
+      <Segment vertical>
+        <Grid padded centered>
+          {recents}
+        </Grid>
+      </Segment>
+    </Container>
+  );
+};
+
+export const pageQuery = graphql`
+  query TemplateBlogPost($slug: String!) {
+  post: markdownRemark(fields: {slug: {eq: $slug}}) {
+    html
+    excerpt
+    timeToRead
+    fields {
+      slug
+    }
+    frontmatter {
+      author {
+        id
+        bio
+        twitter
+        avatar {
+          children {
+            ... on ImageSharp {
+              responsiveResolution(width: 80, height: 80, quality: 100) {
+                src
+                srcSet
+              }
+            }
+          }
+        }
+      }
+      title
+      updatedDate(formatString: "MMM D, YYYY")
+      image {
+        children {
+          ... on ImageSharp {
+            responsiveResolution(width: 900, height: 300, quality: 100) {
+              src
+              srcSet
+            }
+          }
+        }
+      }
+    }
+  }
+  recents: allMarkdownRemark(
+    filter: {
+      fields: {slug: {ne: $slug}}
+      frontmatter: {draft: {ne: true}},
+      fileAbsolutePath: {regex: "/blog/"},
+    },
+    sort: {order: DESC, fields: [frontmatter___updatedDate]},
+    limit: 4
+  ) {
+    edges {
+      node {
+        fields {
+          slug
+        }
+        timeToRead
+        frontmatter {
+          title
+          image {
+            children {
+              ... on ImageSharp {
+                responsiveResolution(width: 300, height: 100) {
+                  src
+                  srcSet
+                }
+              }
+            }
+          }
+          author {
+            id
+            avatar {
+              children {
+                ... on ImageSharp {
+                  responsiveResolution(width: 36, height: 36) {
+                    src
+                    srcSet
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`;
